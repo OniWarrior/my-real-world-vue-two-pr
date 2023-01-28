765a508417a36343578bc8d63042fca496989b67
Stephen Aranda <aranda.stephen88@gmail.com>
2023-01-27 21:42:40 -0600

    removed es prettier and added component

--- real-world-vue/.eslintrc.js
+++ real-world-vue/.eslintrc.js
@@ -3,13 +3,10 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ["plugin:vue/essential", "eslint:recommended"],
   parserOptions: {
     parser: "@babel/eslint-parser",
+    requiredConfigFile: false,
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
