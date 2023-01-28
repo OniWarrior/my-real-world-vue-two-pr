98333fce20290eaa7640d6e2e3d721b8f6e514e7
Stephen Aranda <aranda.stephen88@gmail.com>
2023-01-27 22:37:25 -0600

    added name for route

--- real-world-vue/src/router/index.js
+++ real-world-vue/src/router/index.js
@@ -14,7 +14,7 @@ const routes = [
   },
   {
     path: "/event",
-    name: "about",
+    name: "event-show",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
