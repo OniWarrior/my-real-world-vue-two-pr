3cc7a0c6049b0d4c6277e80a464fa760b1f51608
Stephen Aranda <aranda.stephen88@gmail.com>
2023-02-05 21:58:42 -0600

    imported and nested NavBar into app comp

--- real-world-vue/src/App.vue
+++ real-world-vue/src/App.vue
@@ -1,8 +1,21 @@
 <template>
   <div id="app">
+    <NavBar />
     <router-view />
   </div>
 </template>
+
+<script>
+import NavBar from "./components/NavBar.vue";
+
+export default {
+  component: {
+    NavBar,
+  },
+};
+</script>
+
+
 
 <style>
 html {
