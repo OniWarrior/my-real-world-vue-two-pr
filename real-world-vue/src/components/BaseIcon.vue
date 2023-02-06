a4e0c2ddb5a25ce2aab64ea009d6bf54d8a4ef1e
Stephen Aranda <aranda.stephen88@gmail.com>
2023-02-06 15:18:31 -0600

    added scoped styles

--- real-world-vue/src/components/BaseIcon.vue
+++ real-world-vue/src/components/BaseIcon.vue
@@ -31,4 +31,19 @@ export default {
 
 
 <style scoped>
+.icon-wrapper {
+  display: inline-flex;
+  align-items: center;
+  color: rgba(0, 0, 0, 0.4);
+  font-size: 1rem;
+  font-weight: 600;
+  margin-right: 6px;
+}
+.icon {
+  stroke: currentColor;
+  stroke-width: 2;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  fill: none;
+}
 </style>
