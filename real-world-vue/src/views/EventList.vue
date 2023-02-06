9541a5e6a90a9e55661710fe4456440eb17138a0
Stephen Aranda <aranda.stephen88@gmail.com>
2023-02-05 21:44:24 -0600

    Changed event link location

--- real-world-vue/src/views/EventList.vue
+++ real-world-vue/src/views/EventList.vue
@@ -1,15 +1,12 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-    <EventCard />
-    <router-link :to="{ name: 'event-show', params: { id: '1' } }"
-      >Show Event #1</router-link
-    >
+    <EventCard></EventCard>
   </div>
 </template>
 
 <script>
-import EventCard from ".components/EventCard.vue";
+import EventCard from "../components/EventCard.vue";
 
 export default {
   components: {
