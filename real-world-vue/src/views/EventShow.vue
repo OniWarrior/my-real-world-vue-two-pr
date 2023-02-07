408964f211f60b14fd4e2bbbb0bc7d5682df82be
Stephen Aranda <aranda.stephen88@gmail.com>
2023-02-06 20:38:19 -0600

    made api call for event

--- real-world-vue/src/views/EventShow.vue
+++ real-world-vue/src/views/EventShow.vue
@@ -1,9 +1,25 @@
 <template>
-  <h1>Showing event #{{ id }}</h1>
+  <h1>Showing event {{ event.title }}</h1>
 </template>
 
 <script>
+import EventService from "../services/EventService";
+
 export default {
   props: ["id"],
+  data() {
+    return {
+      events: {},
+    };
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then((response) => {
+        this.event = response.data;
+      })
+      .catch((error) => {
+        console.log("there was an error " + error.response);
+      });
+  },
 };
 </script>
