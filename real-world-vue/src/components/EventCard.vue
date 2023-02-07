aca9376d7b8815e9449809de9ecf9c0e6d7c35d9
Stephen Aranda <aranda.stephen88@gmail.com>
2023-02-06 20:19:37 -0600

    created prop for event object

--- real-world-vue/src/components/EventCard.vue
+++ real-world-vue/src/components/EventCard.vue
@@ -13,19 +13,8 @@
 
 <script>
 export default {
-  data() {
-    return {
-      event: {
-        id: 1,
-        title: "Beach Cleanup",
-        date: "Tue January 30,2023",
-        time: "6:00",
-        attendees: [
-          { id: "abc123", name: "Stephen Ryan" },
-          { id: "def321", name: "Dr. Manhattan" },
-        ],
-      },
-    };
+  props: {
+    event: Object,
   },
 };
 </script>
