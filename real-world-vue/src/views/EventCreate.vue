976d69628a44f5f062014b43d2f7f7dc340dbfc9
Stephen Aranda <aranda.stephen88@gmail.com>
2023-02-07 10:42:57 -0600

    created el to print number of category length

--- real-world-vue/src/views/EventCreate.vue
+++ real-world-vue/src/views/EventCreate.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>Create Event, {{ user.name }}</h1>
     <p>This event was created by {{ user.id }}</p>
+    <p>There are {{ catLength }}</p>
     <ul>
       <li v-for="cat in categories" :key="cat">{{ cat }}</li>
     </ul>
