3a3bc3d33ddbe0ad09d6c1ae0b38db38ce66a67e
Stephen Aranda <aranda.stephen88@gmail.com>
2023-02-07 10:37:26 -0600

    printed categories from state. used spread op

--- real-world-vue/src/views/EventCreate.vue
+++ real-world-vue/src/views/EventCreate.vue
@@ -1,20 +1,19 @@
 <template>
   <div>
-    <h1>Create Event, {{ userName }}</h1>
-    <p>This event was created by {{ userName }}</p>
+    <h1>Create Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.id }}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
   </div>
 </template>
 
 
 <script>
+import { mapState } from "vuex";
 export default {
   computed: {
-    userName() {
-      return this.$store.state.user.name;
-    },
-    userId() {
-      return this.$store.state.user.id;
-    },
+    ...mapState(["user", "categories"]),
   },
 };
 </script>
