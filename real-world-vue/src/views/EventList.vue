ad126880d759e5dd983a3f046d6bf45fb59a535e
Stephen Aranda <aranda.stephen88@gmail.com>
2023-04-13 13:55:45 -0500

    nested event card into eventlist

--- real-world-vue/src/views/EventList.vue
+++ real-world-vue/src/views/EventList.vue
@@ -2,9 +2,9 @@
     <div>
         <h1>Event Listings</h1>
         <EventCard/>
-        <p>
-            <router-link :to="{name: 'event-show',params: {id:'1'}}">First Event</router-link>
-        </p>
+        
+          
+        
      
         
     </div>
